3e0f71a8ea81b1b4ac101fd7f265889835af59bc
alicjasiudak <44605461+alicjasiudak@users.noreply.github.com>
2023-04-15 14:12:13 +0200

    sphere script

--- Assets/Scripts/SphereController.cs
+++ Assets/Scripts/SphereController.cs
@@ -1,9 +1,15 @@
 using System.Collections;
 using System.Collections.Generic;
 using UnityEngine;
+using LEGOWirelessSDK;
 
 public class SphereController : MonoBehaviour
 {
+    public GameObject sphere;
+    public ForceSensor forceSensor;
+    private RigidBody rigidBody;
+
+
     // Start is called before the first frame update
     void Start()
     {
